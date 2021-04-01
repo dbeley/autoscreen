a2e30062354a6d911aced8bd82137b5636f54862
dbeley <6568955+dbeley@users.noreply.github.com>
2021-04-02 01:43:13 +0200

    Add hostname.

--- autoscreen.sh
+++ autoscreen.sh
@@ -2,7 +2,8 @@
 set -eEu -o pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
 TODAY="$(date +%Y-%m-%d)"
+HOSTNAME="$(cat /proc/sys/kernel/hostname)"
 DESTINATION_DIR="$HOME/autoscreen/$TODAY"
 
 mkdir -p "$DESTINATION_DIR"
-maim -m 10 "$DESTINATION_DIR/autoscreen_$(date +%s).png"
+maim -m 10 "$DESTINATION_DIR/$HOSTNAME_autoscreen_$(date +%s).png"
