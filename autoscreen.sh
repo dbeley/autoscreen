d6aaef67f535b9c1ab3be15b29cbd00bd62e49d8
dbeley <6568955+dbeley@users.noreply.github.com>
2023-01-30 22:09:31 +0100

    Fix paths

--- autoscreen.sh
+++ autoscreen.sh
@@ -3,7 +3,7 @@ set -eEu -o pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
 TODAY="$(date +%Y-%m-%d)"
 HOSTNAME="$(cat /proc/sys/kernel/hostname)"
-DESTINATION_DIR="$HOME/autoscreen/$TODAY"
+DESTINATION_DIR="$HOME/Nextcloud/07_Images/01_autoscreen/$TODAY"
 
 mkdir -p "$DESTINATION_DIR"
 maim -m 10 "${DESTINATION_DIR}/${HOSTNAME}_autoscreen_$(date +%Y-%m-%d_%H:%M:%S_%s).png"
--- autoscreen_wayland.sh
+++ autoscreen_wayland.sh
@@ -2,8 +2,7 @@
 set -eEu -o pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
 TODAY="$(date +%Y-%m-%d)"
-# HOSTNAME="$(cat /proc/sys/kernel/hostname)"
-HOSTNAME="MeLE"
+HOSTNAME="$(cat /proc/sys/kernel/hostname)"
 DESTINATION_DIR="$HOME/Nextcloud/07_Images/01_autoscreen/$TODAY"
 
 mkdir -p "$DESTINATION_DIR"
